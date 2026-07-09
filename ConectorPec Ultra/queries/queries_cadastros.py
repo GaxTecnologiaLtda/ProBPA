866e934148defe5e900055c69c8b2f43f09557d7
Gabriel Adelino <gabriel@MacBook-Air-de-Gabriel.local>
2026-07-08 23:04:57 -0300

    feat: add address, phone and ACS data to cidadania_base query

--- ConectorPec Ultra/queries/queries_cadastros.py
+++ ConectorPec Ultra/queries/queries_cadastros.py
@@ -11,15 +11,23 @@ SELECT
     cid.no_mae AS nome_mae,
     fat_cid.co_dim_sexo AS sexo,
     cid.nu_micro_area AS micro_area,
+    cid.ds_logradouro AS logradouro,
+    cid.nu_numero AS numero,
+    cid.ds_complemento AS complemento,
+    cid.no_bairro AS bairro,
+    cid.ds_cep AS cep,
+    cid.nu_telefone_celular AS telefone_celular,
+    cid.nu_telefone_contato AS telefone_contato,
     dim_eq.nu_ine AS ine,
     dim_eq.no_equipe AS nome_equipe,
     dim_us.nu_cnes AS cnes,
     dim_us.no_unidade_saude AS nome_unidade_saude,
-    NULL AS nome_profissional_vinc
+    dim_prof.no_profissional AS nome_profissional_vinc
 FROM tb_cidadao cid
 JOIN tb_fat_cidadao_pec fat_cid ON cid.co_seq_cidadao = fat_cid.co_cidadao
 LEFT JOIN tb_dim_equipe dim_eq ON fat_cid.co_dim_equipe_vinc = dim_eq.co_seq_dim_equipe
 LEFT JOIN tb_dim_unidade_saude dim_us ON fat_cid.co_dim_unidade_saude_vinc = dim_us.co_seq_dim_unidade_saude
+LEFT JOIN tb_dim_profissional dim_prof ON fat_cid.co_dim_profissional_vinc = dim_prof.co_seq_dim_profissional
 WHERE fat_cid.st_faleceu = 0
 """
 
